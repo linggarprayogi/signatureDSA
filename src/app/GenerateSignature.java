64afd3383a0c039adcc36c4da4f5c952f25f3743
linggarprayogi <linggar.prayogi@gmail.com>
2022-04-12 15:14:57 +0700

    export key to file

--- src/app/GenerateSignature.java
+++ src/app/GenerateSignature.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.KeyPair;
@@ -7,6 +8,7 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Calendar;
@@ -18,26 +20,38 @@ public class GenerateSignature {
 		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
 
 		// Initializing the key pair generator
-		keyPairGen.initialize(1024);
+		keyPairGen.initialize(2048);
 
 		// Generate the pair of keys
-		KeyPair    pair      = keyPairGen.generateKeyPair();
+		KeyPair    pair    = keyPairGen.generateKeyPair();
 
 		// Getting the privatekey from the key pair
-		PrivateKey privKey   = pair.getPrivate();
+//		File       privateKeyFile = new File("privateDsa.key");
+//		byte[]     privKeyBytes   = Files.readAllBytes(privateKeyFile.toPath());
+		PrivateKey privKey = KeyFactory.getInstance("DSA")
+				.generatePrivate(new PKCS8EncodedKeySpec(pair.getPrivate().getEncoded()));
+
+		try (FileOutputStream fos = new FileOutputStream("privateDsa.key")) {
+			fos.write(privKey.getEncoded());
+		}
 
 		/* Get the public key from the encoded byte array */
-		PublicKey  pubKey    = KeyFactory.getInstance("DSA")
+//		File       publicKeyFile  = new File("publicDsa.key");
+//		byte[]     publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+		PublicKey pubKey = KeyFactory.getInstance("DSA")
 				.generatePublic(new X509EncodedKeySpec(pair.getPublic().getEncoded()));
-		String     pubKeyStr = Base64.getEncoder().encodeToString(pubKey.getEncoded());
+//		String    pubKeyStr = Base64.getEncoder().encodeToString(pubKey.getEncoded());
+//
+		try (FileOutputStream fos = new FileOutputStream("publicDsa.key")) {
+			fos.write(pubKey.getEncoded());
+		}
 
 		// Creating a Signature object
-		Signature  sign      = Signature.getInstance("SHA256withDSA");
+		Signature sign = Signature.getInstance("SHA256withDSA");
 
 		// Initializing the signature
 		sign.initSign(privKey);
 
-//		Date     currentDate = new Date();
 		Calendar calendar = Calendar.getInstance(); // gets a calendar using the default time zone and locale.
 		calendar.add(Calendar.SECOND, 60);
 		Long   unixTime = calendar.getTimeInMillis();
@@ -52,7 +66,7 @@ public class GenerateSignature {
 		byte[] signature = sign.sign();
 
 		System.out.println("Signature : " + Base64.getEncoder().encodeToString(signature) + " " + signature);
-		System.out.println("Public key : " + pubKeyStr);
+		System.out.println("Public key : " + Base64.getEncoder().encodeToString(pubKey.getEncoded()));
 		System.out.println("Private key : " + privKey);
 		System.out.println(
 				"Output : " + new String(data, StandardCharsets.UTF_8) + Base64.getEncoder().encodeToString(signature));
--- src/app/VerifySignature.java
+++ src/app/VerifySignature.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.Signature;
@@ -11,16 +13,18 @@ import java.util.Date;
 public class VerifySignature {
 
 	public static void main(String[] args) throws Exception {
-		verifySignature("INV001|1649737886047|", "MCwCFCY5vxMRIRDQbQ07qUrbAt5KlT4CAhRExtV9QjKps65bqORU4VdDWv6zWA==",
-				"MIIBtzCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYQAAoGAECwvalyMyj0NkIf/IXkfcxCeggRdNOpOFFX7NmDEu/BcQ3Cfy45srItHCm+O44iJcSQnCrvrA2bASMd0TSKZbFSFgO5gLczeUKUWha5TT7DtCXxBy/fNznDe/con7J28y3BDvWiuYRf+7e+LbQxFdoLV55f5Hx5ez9xDPQQMdRs=");
+		File   publicKeyFile  = new File("publicDsa.key");
+		byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
+
+		verifySignature("INV001|1649751179779|",
+				"MD0CHEWIU/2OlWnuNJeYZU/1JZP8Bdt7M4O1RWKHYdoCHQC2kTn9upAiwFSzsZ9p+jhTvfrtxm6dPwHJVZyM", publicKeyBytes);
 	}
 
-	public static void verifySignature(String data, String signature, String publickey) throws Exception {
+	public static void verifySignature(String data, String signature, byte[] publickey) throws Exception {
 		// Creating a Signature object
 		Signature sign   = Signature.getInstance("SHA256withDSA");
 
-		PublicKey pubKey = KeyFactory.getInstance("DSA")
-				.generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publickey)));
+		PublicKey pubKey = KeyFactory.getInstance("DSA").generatePublic(new X509EncodedKeySpec(publickey));
 		sign.initVerify(pubKey);
 		System.out.println(pubKey);
 		byte[] dataByte = data.getBytes(StandardCharsets.UTF_8);
@@ -32,7 +36,7 @@ public class VerifySignature {
 		boolean bool        = sign.verify(signatureByte);
 
 		Date    currentDate = new Date();
-		if (currentDate.compareTo(new Date(1649737886047l)) > 0) {
+		if (currentDate.compareTo(new Date(1649751179779l)) > 0) {
 			System.out.println("Expire Date");
 		}
 
